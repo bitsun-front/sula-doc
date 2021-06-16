4eceaed3b4a3eef65d4d56824e307453706e6a07
肖明辉 <xiao.xmh@bitsun-inc.com>
2021-06-16 22:19:34 +0800

    初始化

--- .history/.umirc_20210616221911.ts
+++ .history/.umirc_20210616221911.ts
@@ -1,8 +1,8 @@
 /*
  * @Author: your name
  * @Date: 2021-06-16 22:11:31
- * @LastEditTime: 2021-06-16 22:19:27
- * @LastEditors: Please set LastEditors
+ * @LastEditTime: 2021-06-16 22:19:11
+ * @LastEditors: your name
  * @Description: In User Settings Edit
  * @FilePath: \sula-doc\.umirc.ts
  */
@@ -12,9 +12,6 @@ export default defineConfig({
   title: 'sula-doc',
   plublicPath: '/sula-doc/',
   base: '/sula-doc/',
-  history: {
-    type: 'hash',
-  },
   favicon:
     'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
   logo: 'https://user-images.githubusercontent.com/9554297/83762004-a0761b00-a6a9-11ea-83b4-9c8ff721d4b8.png',
